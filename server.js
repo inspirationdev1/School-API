cfac3aaf35808cef192b472a82fd6590318588ad
inspirationdev1 <inspirationdev1@gmail.com>
2026-03-07 15:33:36 +0530

    port changed

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/", (req, res) => {
   res.send("School API is running 🚀");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log("Server is running at port =>",PORT)
 })
