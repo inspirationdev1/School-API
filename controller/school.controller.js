4dabbc6fe9b84d5f3fe388744b657b5cd3668398
inspirationdev1 <inspirationdev1@gmail.com>
2026-03-07 23:01:15 +0530

    login testing

--- controller/school.controller.js
+++ controller/school.controller.js
@@ -75,36 +75,37 @@ module.exports = {
 
     },
     loginSchool: async (req, res) => {
-        School.find({ email: req.body.email }).then(resp => {
-            if (resp.length > 0) {
-                const isAuth = bcrypt.compareSync(req.body.password, resp[0].password);
-                if (isAuth) {   
-                    const token = jwt.sign(
-                        {
-                            id: resp[0]._id,
-                            schoolId:resp[0]._id,
-                            school_name: resp[0].school_name,
-                            owner_name:resp[0].owner_name,
-                            image_url: resp[0].school_image,
-                            role:'SCHOOL'
-                        }, jwtSecret );
+        // School.find({ email: req.body.email }).then(resp => {
+        //     if (resp.length > 0) {
+        //         const isAuth = bcrypt.compareSync(req.body.password, resp[0].password);
+        //         if (isAuth) {   
+        //             const token = jwt.sign(
+        //                 {
+        //                     id: resp[0]._id,
+        //                     schoolId:resp[0]._id,
+        //                     school_name: resp[0].school_name,
+        //                     owner_name:resp[0].owner_name,
+        //                     image_url: resp[0].school_image,
+        //                     role:'SCHOOL'
+        //                 }, jwtSecret );
 
-                   res.header("Authorization", token);
-                   res.status(200).json({ success: true, message: "Success Login", 
-                    user: {
-                         id: resp[0]._id, 
-                         owner_name:resp[0].owner_name, 
-                         school_name: resp[0].school_name,
-                          image_url: resp[0].school_image, 
-                          role: "SCHOOL" } })
-                }else {
-                    res.status(401).json({ success: false, message: "Password doesn't match." })
-                }
+        //            res.header("Authorization", token);
+        //            res.status(200).json({ success: true, message: "Success Login", 
+        //             user: {
+        //                  id: resp[0]._id, 
+        //                  owner_name:resp[0].owner_name, 
+        //                  school_name: resp[0].school_name,
+        //                   image_url: resp[0].school_image, 
+        //                   role: "SCHOOL" } })
+        //         }else {
+        //             res.status(401).json({ success: false, message: "Password doesn't match." })
+        //         }
 
-            } else {
-                res.status(401).json({ success: false, message: "Email not registerd." })
-            }
-        })
+        //     } else {
+        //         res.status(401).json({ success: false, message: "Email not registerd." })
+        //     }
+        // })
+        res.status(200).json({success:true, message:"Testing Successfullyr"})
     },
     getSchoolOwnData: async(req, res)=>{
         const id = req.user.id;
