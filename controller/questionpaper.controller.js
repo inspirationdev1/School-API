51bf506cec02ba45c9bc54f718148ddfbdb33c68
inspirationdev1 <inspirationdev1@gmail.com>
2026-03-20 02:52:32 +0530

    pdf and image save change

--- controller/questionpaper.controller.js
+++ controller/questionpaper.controller.js
@@ -26,32 +26,10 @@ module.exports = {
             let fileType = "";
 
             if (file) {
-
-            //     let oldPath = file.filepath;
-
                 fileName = file.originalFilename.replace(/\s/g, "_");
-
                 fileType = path.extname(fileName); // .pdf / .jpg / .png
-
-            //     let newPath = path.join(
-            //         __dirname,
-            //         "../../frontend/public/uploads/questionpapers/",
-            //         fileName
-            //     );
-            //     // let newPath = path.join(__dirname, '../../frontend/public/images/uploaded/student', '/', originalFileName);
-            //     // let photoData = fs.readFileSync(oldPath);
-            //     //                     fs.writeFile(newPath, photoData, function (err) {
-            //     console.log("oldPath:", oldPath);
-            //     console.log("newPath:", newPath);
-
-            //     try {
-            //         fs.copyFileSync(oldPath, newPath);
-            //         console.log("oldPath:", oldPath);
-            //         console.log("newPath:", newPath);
-            //     } catch (error) {
-            //         return res.status(500).send({ success: false, message: error.message });
-            //     }
             }
+            
             const photo = files.image[0];
             let oldPath = photo.filepath;
             let originalFileName = photo.originalFilename.replace(" ", "_")
@@ -71,7 +49,7 @@ module.exports = {
                     class: fields.class[0],
                     examination: fields.examination[0],
                     marksLimit: fields.marksLimit[0],
-                    fileName: fileName,
+                    fileName: fileName||originalFileName,
                     fileType: fileType,
                     school: req.user.id
                 });
@@ -94,35 +72,7 @@ module.exports = {
 
             })
 
-            // const newQuestionpaper = new Questionpaper({
-            //     name: fields.name[0],
-            //     description: fields.description[0],
-            //     date: fields.date[0],
-            //     subject: fields.subject[0],
-            //     teacher: fields.teacher[0],
-            //     class: fields.class[0],
-            //     examination: fields.examination[0],
-            //     marksLimit: fields.marksLimit[0],
-            //     fileName: fileName,
-            //     fileType: fileType,
-            //     school: req.user.id
-            // });
-
-            // newQuestionpaper
-            //     .save()
-            //     .then(() => {
-            //         res.status(200).send({
-            //             success: true,
-            //             message: "Questionpaper Added Successfully."
-            //         });
-            //     })
-            //     .catch((e) => {
-            //         console.log(e);
-            //         res.status(500).send({
-            //             success: false,
-            //             message: "Failure in Questionpaper."
-            //         });
-            //     });
+            
         });
     }
 
