9145c22959ab507ce094c51345f948d3f401dde7
inspirationdev1 <inspirationdev1@gmail.com>
2026-04-10 12:05:15 +0530

    Duplicates logic implemented

--- controller/period.controller.js
+++ controller/period.controller.js
@@ -4,6 +4,25 @@ const Period = require('../model/period.model');
 exports.createPeriod = async (req, res) => {
   try {
     const schoolId = req.user.schoolId;
+    const classId = req.body.class;
+    const sectionId = req.body.section;
+    const subjectId = req.body.subject;
+    const teacherId = req.body.teacher;
+
+    let paramsDuplicate = {
+      school: schoolId,
+      class: classId,
+      section: sectionId,
+      subject: subjectId,
+      teacher: teacherId,
+      id: ""
+    }
+    const isDuplicate = await checkDuplicate(paramsDuplicate);
+    if (isDuplicate) {
+      console.log("isDuplicate", isDuplicate);
+      res.status(500).json({ success: false, message: 'Error Duplicatiing period' });
+      return;
+    }
 
 
     const newPeriod = new Period({ ...req.body, school: schoolId });
@@ -20,16 +39,16 @@ exports.createPeriod = async (req, res) => {
     for (const item of periodSeq) {
       subseq += 1;
       const id = item._id;
-      const subjectkey = "subject"+subseq;
+      const subjectkey = "subject" + subseq;
       item.subjectkey = subjectkey;
       const updatedPeriodSeq = await Period.findOneAndUpdate({ _id: id }, { $set: { ...item } });
-      console.log("updatedPeriodSeq",updatedPeriodSeq);
+      console.log("updatedPeriodSeq", updatedPeriodSeq);
       // code
     }
-    
-    res.status(201).json({ message: 'Period assigned successfully', period: newPeriod });
+
+    res.status(201).json({ success: true, message: 'Period assigned successfully', period: newPeriod });
   } catch (error) {
-    res.status(500).json({ message: 'Error creating period', error });
+    res.status(500).json({ success: false, message: 'Error creating period', error });
     console.log("Error", error)
   }
 };
@@ -93,29 +112,32 @@ exports.updatePeriod = async (req, res) => {
 
   try {
 
+    const schoolId = req.user.schoolId;
 
     const id = req.params.id;
-
-
-    updatedPeriod = await Period.findOneAndUpdate({ _id: id }, { $set: { ...req.body } });
-
     const classId = req.body.class;
     const sectionId = req.body.section;
     const subjectId = req.body.subject;
-    
-  //   let paramsDuplicate = {
-  //     class: classId,
-  //     section: sectionId,
-  //     subject: subjectId
-  //   }
-  //   const isDuplicate = await checkDuplicate(paramsDuplicate);
-  //  if (isDuplicate) {
-  //   console.log("checkDuplicate",isDuplicate);
-  //   res.status(500).json({ message: 'Error Duplicatiing period' });
-  //   return;
-  //  }
+    const teacherId = req.body.teacher;
 
-   let params = {
+    let paramsDuplicate = {
+      school: schoolId,
+      class: classId,
+      section: sectionId,
+      subject: subjectId,
+      teacher: teacherId,
+      id: id,
+      starttime: req.body.starttime || "00:00"
+    }
+    const isDuplicate = await checkDuplicate(paramsDuplicate);
+    if (isDuplicate) {
+      console.log("isDuplicate", isDuplicate);
+      res.status(500).json({ success: false, message: 'Error Duplicatiing period' });
+      return;
+    }
+
+    updatedPeriod = await Period.findOneAndUpdate({ _id: id }, { $set: { ...req.body } });
+    let params = {
       class: classId,
       section: sectionId
     }
@@ -125,16 +147,16 @@ exports.updatePeriod = async (req, res) => {
     for (const item of periodSeq) {
       subseq += 1;
       const id = item._id;
-      const subjectkey = "subject"+subseq;
+      const subjectkey = "subject" + subseq;
       item.subjectkey = subjectkey;
       const updatedPeriodSeq = await Period.findOneAndUpdate({ _id: id }, { $set: { ...item } });
-      console.log("updatedPeriodSeq",updatedPeriodSeq);
+      console.log("updatedPeriodSeq", updatedPeriodSeq);
       // code
     }
 
-    res.status(200).json({ message: 'Period updated successfully', period: updatedPeriod });
+    res.status(200).json({ success: true, message: 'Period updated successfully', period: updatedPeriod });
   } catch (error) {
-    res.status(500).json({ message: 'Error updating period', error });
+    res.status(500).json({ success: false, message: 'Error updating period', error });
   }
 };
 
@@ -149,13 +171,75 @@ exports.deletePeriod = async (req, res) => {
   }
 };
 
-checkDuplicate = async (params) =>{
+const toMinutes = (time) => {
+  const [h, m] = time.split(":").map(Number);
+  return h * 60 + m;
+};
+
+checkDuplicate = async (paramsDuplicate) => {
+
   let isDuplicate = false;
-  const periodDuplicate = await Period.find(params).sort({ timeseq: 1 }).lean();
-  console.log("periodDuplicate", periodDuplicate);
-  
-  console.log("Test-1");
-  isDuplicate=true;
+
+  let existingId = paramsDuplicate.id;
+  let currenttime = paramsDuplicate.starttime;
+
+
+  try {
+    let params = {
+      class: paramsDuplicate.class,
+      section: paramsDuplicate.section
+    }
+    const periodDuplicate = await Period.find(params).sort({ timeseq: 1 }).lean();
+    console.log("periodDuplicate", periodDuplicate);
+    if (periodDuplicate.length > 0) {
+      let subjectId = paramsDuplicate.subject || "";
+      isDuplicate = periodDuplicate.some(
+        item => item._id.toString() !== existingId && item.subject.toString() === subjectId.toString()
+      );
+      if (isDuplicate) {
+        return isDuplicate;
+      }
+
+      for (const item of periodDuplicate) {
+        const startminutes = toMinutes(item.starttime);
+        const endminutes = toMinutes(item.endtime);
+        const currentminutes = toMinutes(currenttime);
+        if (item._id.toString() !== existingId) {
+          if (currentminutes >= startminutes && currentminutes <= endminutes) {
+            isDuplicate = true;
+            return isDuplicate;
+            // break;
+          }
+        }
+      }
+
+    //   let paramsTeacher = {
+    //   teacher: paramsDuplicate.teacher
+    // }
+    // const periodTeacherData = await Period.find(paramsTeacher).sort({ timeseq: 1 }).lean();
+    // console.log("periodTeacherData", periodTeacherData);
+    // for (const item of periodTeacherData) {
+    //     const startminutes = toMinutes(item.starttime);
+    //     const endminutes = toMinutes(item.endtime);
+    //     const currentminutes = toMinutes(currenttime);
+    //     if (item._id.toString() !== existingId) {
+    //       if (currentminutes >= startminutes && currentminutes <= endminutes) {
+    //         isDuplicate = true;
+    //         return isDuplicate;
+    //         // break;
+    //       }
+    //     }
+    //   }
+
+
+
+
+    }
+  } catch (error) {
+    console.log("Duplicate function", error.message);
+  }
   return isDuplicate;
 }
+
+
 
