85f6ad4195dbf27ce11de40c10e5faeb7f66e934
inspirationdev1 <inspirationdev1@gmail.com>
2026-04-13 09:51:12 +0530

    Many problem fixed on sunday

--- model/expense.model.js
+++ model/expense.model.js
@@ -13,6 +13,7 @@ const expenseSchema = new mongoose.Schema({
     expenseTime: { type: Date, required: true, },
     employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
     status: { type: String, default: 'valid' },
+    expenseAmount: { type: Number, default: 0 },
     remarks: { type: String, default: '' },
     year: { type: Number, default: new Date().getFullYear() },
     createdAt: { type: Date, default: new Date() }
