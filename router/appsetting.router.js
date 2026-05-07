9da9f3de222380027bbdfe484f84033c5697e267
inspirationdev1 <inspirationdev1@gmail.com>
2026-05-07 15:12:16 +0530

    Attendance Report Added to Teacher

--- router/appsetting.router.js
+++ router/appsetting.router.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../auth/auth');
 const { createAppsetting, getAllAppsettings, getAppsettingWithId, updateAppsettingWithId, deleteAppsettingWithId } = require("../controller/appsetting.controller");
 
 router.post("/create",authMiddleware(['SCHOOL','USER']), createAppsetting);
-router.get("/fetch-all",authMiddleware(['SCHOOL','USER']),getAllAppsettings);
+router.get("/fetch-all",authMiddleware(['SCHOOL','USER','TEACHER','STUDENT','PARENT']),getAllAppsettings);
 router.get("/fetch-single/:id",authMiddleware(['SCHOOL','USER']),  getAppsettingWithId);
 router.patch("/update/:id",authMiddleware(['SCHOOL','USER']), updateAppsettingWithId);
 router.delete("/delete/:id",authMiddleware(['SCHOOL','USER']), deleteAppsettingWithId);
--- router/questionpaper.router.js
+++ router/questionpaper.router.js
@@ -4,12 +4,12 @@ const authMiddleware = require('../auth/auth');
 const { newQuestionpaper,  getQuestionpaperByClass, updateQuestionpaperWithId, deleteQuestionpaperById, getQuestionpaperById, getAllQuestionpapers, getQuestionpaperWithQuery} = require("../controller/questionpaper.controller");
 
 
-router.post("/new", authMiddleware(['SCHOOL','USER']),newQuestionpaper);
+router.post("/new", authMiddleware(['SCHOOL','USER','TEACHER']),newQuestionpaper);
 router.get("/all", authMiddleware(['SCHOOL','USER','TEACHER','STUDENT','PARENT']), getAllQuestionpapers);
 router.get("/fetch-with-query",authMiddleware(['SCHOOL','USER','STUDENT','TEACHER','PARENT']),getQuestionpaperWithQuery);
 router.get("/fetch-class/:classId",authMiddleware(['SCHOOL','STUDENT','TEACHER','PARENT','USER']),  getQuestionpaperByClass);
 router.get('/single/:id',authMiddleware(['SCHOOL','USER','TEACHER']), getQuestionpaperById );
 router.patch("/update/:id",authMiddleware(['SCHOOL','USER','TEACHER']), updateQuestionpaperWithId);
-router.delete("/delete/:id",authMiddleware(['SCHOOL','USER']),  deleteQuestionpaperById);
+router.delete("/delete/:id",authMiddleware(['SCHOOL','USER','TEACHER']),  deleteQuestionpaperById);
 
 module.exports = router;
