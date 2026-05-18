2cd2321dd7aa2dada685a8938e93586890ab9ef3
inspirationdev1 <inspirationdev1@gmail.com>
2026-05-18 22:59:43 +0530

    Added Whatsapp messages option

--- controller/whatsapp.controller.js
+++ controller/whatsapp.controller.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const twilio = require("twilio");
 
 const XLSX = require("xlsx");
 const fs = require("fs");
@@ -21,11 +22,82 @@ const Employee = require("../model/employee.model");
 module.exports = {
 
     send_whatsapp_Multiple: async (req, res) => {
-   
-},
+
+    },
     send_whatsapp: async (req, res) => {
-        
-        
+        try {
+            const schoolId = req.user.schoolId;
+
+            //  const phoneNumbers = ["9550345573", "7674968840"];
+
+            const phone_no = "+91" + req.body?.phone_no
+            const client = require('twilio')(
+                process.env.TWILIO_ACCOUNT_SID,
+                process.env.TWILIO_AUTH_TOKEN
+            );
+
+
+            const message = await client.messages.create({
+                from: process.env.TWILIO_WHATSAPP_NUMBER,
+                contentSid: process.env.TWILIO_CONTENTSID,
+                contentVariables: JSON.stringify({
+                    1: "Message from SMS System",
+                    2: "(Inspiration)"
+                }),
+                to: 'whatsapp:' + phone_no
+            });
+
+
+
+            console.log(message.sid);
+
+            res.status(200).json({
+                success: true,
+                data: [],
+                message: "Whatsapp sent Successfully."
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            res.status(500).json({
+                success: false,
+                message: error.message
+            });
+        }
+    },
+    send_bulk_whatsapp: async (req, res) => {
+        const client = twilio(
+            process.env.TWILIO_ACCOUNT_SID,
+            process.env.TWILIO_AUTH_TOKEN,
+        );
+        // List of recipient numbers
+        const numbers = ["whatsapp:+919550345573", "whatsapp:+917674968840"];
+        for (const to of numbers) {
+            try {
+                const message = await client.messages.create({
+                     from: process.env.TWILIO_WHATSAPP_NUMBER,
+                    to: to,
+                    body: "Hello from Node.js and Twilio WhatsApp 🚀",
+                });
+
+                console.log(`Message sent to ${to}`);
+                console.log(`SID: ${message.sid}`);
+
+                res.status(200).json({
+                success: true,
+                data: [],
+                message: "Whatsapp sent Successfully."
+            });
+            } catch (error) {
+                console.error(`Failed for ${to}`);
+                console.error(error.message);
+                res.status(500).json({
+                success: false,
+                message: error.message
+            });
+            }
+        }
     },
 
 }
--- router/whatsapp.router.js
+++ router/whatsapp.router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require('../auth/auth');
-const { send_whatsapp } = require("../controller/whatsapp.controller");
+const { send_whatsapp,send_bulk_whatsapp } = require("../controller/whatsapp.controller");
 const multer = require("multer");
 
 // store file temporarily
 const upload = multer({ dest: "uploads/" });
 router.post("/send_whatsapp", upload.single("file"),authMiddleware(['SCHOOL','USER']), send_whatsapp);
+router.post("/send_bulk_whatsapp", upload.single("file"),authMiddleware(['SCHOOL','USER']), send_bulk_whatsapp);
+
 module.exports = router;
 
